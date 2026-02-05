11dcf06690db903ad685aefad6210c1f199bd846
kvkvkv <kvkvkv@student.42.rio>
2026-02-05 12:22:36 -0300

    feat: local build + basic shadow screenshot

---
+++ build.ps1
@@ -0,0 +1,90 @@
+param(
+    [switch]$Release
+)
+
+$ErrorActionPreference = 'Stop'
+
+function Get-VcVars64 {
+    $candidates = @(
+        "$env:ProgramFiles(x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat",
+        "$env:ProgramFiles\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat",
+        "$env:ProgramFiles(x86)\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat",
+        "$env:ProgramFiles\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+    )
+    foreach ($p in $candidates) {
+        if (Test-Path $p) {
+            return $p
+        }
+    }
+    $vswhere = "$env:ProgramFiles(x86)\Microsoft Visual Studio\Installer\vswhere.exe"
+    if (Test-Path $vswhere) {
+        $vs = & $vswhere -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath
+        if ($vs) {
+            $p = Join-Path $vs 'VC\Auxiliary\Build\vcvars64.bat'
+            if (Test-Path $p) {
+                return $p
+            }
+        }
+    }
+    return $null
+}
+
+function Import-VcVars64 {
+    $vcvars = Get-VcVars64
+    if (-not $vcvars) {
+        throw 'vcvars64.bat not found. Install Visual Studio Build Tools or Community with C++.'
+    }
+    & $env:COMSPEC /s /c "`"$vcvars`" & set" 2>$null | ForEach-Object {
+        if ($_ -match '^(.*?)=(.*)$') {
+            Set-Item -Path Env:$($matches[1]) -Value $matches[2]
+        }
+    }
+}
+
+if (-not (Get-Command cl.exe -ErrorAction SilentlyContinue)) {
+    Import-VcVars64
+}
+
+$flags = @('/nologo', '/W4', '/DUNICODE', '/D_UNICODE')
+if ($Release) {
+    $flags += '/O2'
+} else {
+    $flags += @('/Od', '/Zi')
+}
+
+$src = @(
+    'src\main.c',
+    'src\app.c',
+    'src\capture.c',
+    'src\capture_bgra.c',
+    'src\capture_utils.c',
+    'src\shadow.c',
+    'src\shadow_calc.c',
+    'src\shadow_alpha.c',
+    'src\shadow_draw.c',
+    'src\shadow_blur_boxes.c',
+    'src\shadow_blur_h.c',
+    'src\shadow_blur_v.c',
+    'src\shadow_blur_alpha.c',
+    'src\clipboard.c',
+    'src\png.c',
+    'src\png_wic_open.c',
+    'src\png_wic_write.c',
+    'src\path.c',
+    'src\util.c',
+    'src\util_mem.c',
+    'src\util_str.c',
+    'src\util_time.c',
+    'src\util_log.c'
+)
+
+$libs = @(
+    'user32.lib',
+    'gdi32.lib',
+    'dwmapi.lib',
+    'ole32.lib',
+    'shell32.lib',
+    'windowscodecs.lib'
+)
+
+& cl.exe @flags /Fe:shot11.exe @src /link @libs
